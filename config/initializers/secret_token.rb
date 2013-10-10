0bead7e54c1200f45e925075d61e2b0d221d2bf4
Rob Weaver <rweaver@westnet.com.au>
2013-10-11 08:10:38 +1100

    Created Biblehive application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biblehive::Application.config.secret_key_base = 'c440819645608167d8a5f504f585ab4260aeba8d7ba26bce9f2c187a700f31a81fdd6fc6e11e6e65971bc2bf5e440c994848844c6b6ea6e64ffc2bf88371058d'
